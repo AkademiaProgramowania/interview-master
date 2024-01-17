8d664c59d42620c62323bce6e432f94a70e6d0a1
WojciechKazura <wojciech.kazura@wp.pl>
2024-01-17 14:19:14 +0100

    Refactoring.

--- src/main/java/pl/pop/interview/master/question/QuestionService.java
+++ src/main/java/pl/pop/interview/master/question/QuestionService.java
@@ -15,25 +15,25 @@ public class QuestionService {
     }
 
     @PostConstruct
-    private void createStartQuestionAndPutToDB() {
+    private void saveBasicQuestions() {
         if (questionRepository.count() == 0) {
-            questionRepository.saveAll(createQuestion());
+            questionRepository.saveAll(createBasicQuestions());
         }
     }
 
-    private List createQuestion(){
-        ArrayList<Question> questionArrayList = new ArrayList<>();
-        questionArrayList.add(new Question("Czy w Javie istnieje wielokrotne dziedziczenie klas?", YesNo.NO));
-        questionArrayList.add(new Question("Czy można utworzyć obiekt klasy abstrakcyjnej w Javie?", YesNo.NO));
-        questionArrayList.add(new Question("Czy w Javie istnieje automatyczne zbieranie śmieci", YesNo.YES));
-        questionArrayList.add(new Question("Czy interfejs w Javie może zawierać implementacje metod?", YesNo.YES));
-        questionArrayList.add(new Question("Czy w Javie można nadpisywać metody statyczne?", YesNo.NO));
-        questionArrayList.add(new Question("Czy Javę można używać do programowania na platformie Android?", YesNo.YES));
-        questionArrayList.add(new Question("Czy Javie można przekazywać argumenty do metody przez referencję?", YesNo.NO));
-        questionArrayList.add(new Question("Czy w Javie można obsługiwać wielowątkowość?", YesNo.YES));
-        questionArrayList.add(new Question("Czy w Javie można dziedziczyć po więcej niż jednej klasie?", YesNo.NO));
-        questionArrayList.add(new Question("CCzy Javie można używać do tworzenia aplikacji webowych?", YesNo.YES));
-        return questionArrayList;
+    private List<Question> createBasicQuestions(){
+        List<Question> questions = new ArrayList<>();
+        questions.add(new Question("Czy w Javie istnieje wielokrotne dziedziczenie klas?", YesNo.NO));
+        questions.add(new Question("Czy można utworzyć obiekt klasy abstrakcyjnej w Javie?", YesNo.NO));
+        questions.add(new Question("Czy w Javie istnieje automatyczne zbieranie śmieci", YesNo.YES));
+        questions.add(new Question("Czy interfejs w Javie może zawierać implementacje metod?", YesNo.YES));
+        questions.add(new Question("Czy w Javie można nadpisywać metody statyczne?", YesNo.NO));
+        questions.add(new Question("Czy Javę można używać do programowania na platformie Android?", YesNo.YES));
+        questions.add(new Question("Czy Javie można przekazywać argumenty do metody przez referencję?", YesNo.NO));
+        questions.add(new Question("Czy w Javie można obsługiwać wielowątkowość?", YesNo.YES));
+        questions.add(new Question("Czy w Javie można dziedziczyć po więcej niż jednej klasie?", YesNo.NO));
+        questions.add(new Question("Czy Javie można używać do tworzenia aplikacji webowych?", YesNo.YES));
+        return questions;
     }
 
     public void addNewQuestion(QuestionDTO questionDTO) {
