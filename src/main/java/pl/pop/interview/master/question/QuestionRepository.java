48ed0eab9c1af91509ddabc13b6349854877cb55
Konrad <hodon534@gmail.com>
2024-01-25 21:59:06 +0100

    service refactored, mappers extracted to other classes

--- src/main/java/pl/pop/interview/master/question/QuestionRepository.java
+++ src/main/java/pl/pop/interview/master/question/QuestionRepository.java
@@ -1,6 +1,8 @@
 package pl.pop.interview.master.question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 }
--- src/main/java/pl/pop/interview/master/question/QuestionService.java
+++ src/main/java/pl/pop/interview/master/question/QuestionService.java
@@ -9,52 +9,22 @@ import java.util.List;
 @Service
 public class QuestionService {
     private final QuestionRepository questionRepository;
+    private final QuestionMapper questionMapper;
 
-    public QuestionService(QuestionRepository questionRepository) {
+    public QuestionService(QuestionRepository questionRepository, QuestionMapper questionMapper) {
         this.questionRepository = questionRepository;
+        this.questionMapper = questionMapper;
     }
 
-    @PostConstruct
-    private void saveBasicQuestions() {
-        if (questionRepository.count() == 0) {
-            questionRepository.saveAll(createBasicQuestions());
-        }
-    }
-
-    private List<Question> createBasicQuestions(){
-        List<Question> questions = new ArrayList<>();
-        questions.add(new Question("Czy w Javie istnieje wielokrotne dziedziczenie klas?", YesNo.NO));
-        questions.add(new Question("Czy można utworzyć obiekt klasy abstrakcyjnej w Javie?", YesNo.NO));
-        questions.add(new Question("Czy w Javie istnieje automatyczne zbieranie śmieci", YesNo.YES));
-        questions.add(new Question("Czy interfejs w Javie może zawierać implementacje metod?", YesNo.YES));
-        questions.add(new Question("Czy w Javie można nadpisywać metody statyczne?", YesNo.NO));
-        questions.add(new Question("Czy Javę można używać do programowania na platformie Android?", YesNo.YES));
-        questions.add(new Question("Czy Javie można przekazywać argumenty do metody przez referencję?", YesNo.NO));
-        questions.add(new Question("Czy w Javie można obsługiwać wielowątkowość?", YesNo.YES));
-        questions.add(new Question("Czy w Javie można dziedziczyć po więcej niż jednej klasie?", YesNo.NO));
-        questions.add(new Question("Czy Javie można używać do tworzenia aplikacji webowych?", YesNo.YES));
-        return questions;
-    }
 
     public void addNewQuestion(QuestionDTO questionDTO) {
-        Question question = mapToEntity(questionDTO);
+        Question question = questionMapper.mapDtoToEntity(questionDTO);
         questionRepository.save(question);
     }
 
     public List<QuestionDTO> getAllQuestions() {
         return questionRepository.findAll().stream()
-                .map( question -> mapToDto(question) )
+                .map(question -> questionMapper.mapEntityToDto(question))
                 .toList();
-    }
-
-    private Question mapToEntity(QuestionDTO questionDTO) {
-        return new Question(
-                questionDTO.getContent(),
-                questionDTO.getCorrectAnswer()
-        );
-    }
-
-    private QuestionDTO mapToDto(Question question) {
-        return new QuestionDTO( question.getContent(), question.getCorrectAnswer() );
     }
 }
