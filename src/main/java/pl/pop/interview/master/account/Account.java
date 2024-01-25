24a3684b13eccf34a3ce5d84b0a7c8db66e44ccd
Konrad <hodon534@gmail.com>
2024-01-25 21:38:43 +0100

    account refactored

--- src/main/java/pl/pop/interview/master/account/Account.java
+++ src/main/java/pl/pop/interview/master/account/Account.java
@@ -9,18 +9,19 @@ import pl.pop.interview.master.practitioner.Practitioner;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Entity(name = "accounts")
 public class Account {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String email;
     private String password;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Practitioner practitioner;
 
     public Account(String email, String password) {
         this.email = email;
         this.password = password;
     }
 
-    @OneToOne
-    private Practitioner practitioner;
 }
