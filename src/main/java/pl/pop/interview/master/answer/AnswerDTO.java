ea76223b4769ea55e709bdfbad1df001de33613d
orcxus <73879364+andreichernetskii@users.noreply.github.com>
2024-02-05 10:47:28 +0100

    added Practitioner and Question ID's for AnswerController's save() method

--- src/main/java/pl/pop/interview/master/answer/AnswerDTO.java
+++ src/main/java/pl/pop/interview/master/answer/AnswerDTO.java
@@ -12,12 +12,16 @@ public class AnswerDTO {
     private String question;
     private String answer;
     private String result;
+    private Long practitionerId;
+    private Long questionId;
 
     public static AnswerDTO mapToDto(Answer answer) {
         return AnswerDTO.builder()
                 .question(answer.getQuestionContent())
                 .answer(answer.getAnswer())
                 .result(answer.getResult())
+                .practitionerId( answer.getPractitioner().getId() )
+                .questionId( answer.getQuestion().getId() )
                 .build();
     }
 }
